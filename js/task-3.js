46e18230cfd259155bb723dfe6d219079f6cb6e0
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-09-29 17:10:33 +0300

    Added solution to the 3d task.

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+function findLongestWord(string = "") {
+  let allWords = string.split(" ");
+  let longestWord = allWords[0];
+
+  for (let i = 1; i < allWords.length; i += 1) {
+    if (longestWord.length < allWords[i].length) {
+      longestWord = allWords[i];
+    }
+  }
+
+  return longestWord;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// 'jumped'
+
+console.log(findLongestWord("Google do a roll"));
+// 'Google'
+
+console.log(findLongestWord("May the force be with you"));
+// 'force'
