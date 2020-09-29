08fd29531bf6db1274ac802f0547afead5346a37
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-09-29 17:04:53 +0300

    Added solution to the 1st task.

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const getItemsString = function (array) {
+  for (let i = 0; i < array.length; i += 1) {
+    array[i] = `${i + 1} - ${array[i]}`;
+  }
+  let result = array.join("\n");
+  return result;
+};
+
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+
+console.log(getItemsString([5, 10, 15]));
+
+console.log(typeof getItemsString([5, 10, 15]));
